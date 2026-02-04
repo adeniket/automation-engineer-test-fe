b19fd4bb3e9918c6f5825ad2c8088a0e0ceb3f5e
AdenikeDavis <adenikky416@gmail.com>
2026-02-04 20:12:51 +0000

    Add Allure Adapter

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   //watchForFileChanges: false,
-     // pageLoadTimeout: 6000,
-      video: true,
+  // pageLoadTimeout: 6000,
+  video: true,
   e2e: {
     watchForFileChanges: false,
     viewportWidth: 1440,
     viewportHeight: 900,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const { allureCypress } = require("allure-cypress/reporter");
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
     baseUrl: "http://localhost:5173/login",
   },
