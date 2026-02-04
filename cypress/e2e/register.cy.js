7bc576f79d401a3a3a00c3366330a61caa632945
AdenikeDavis <adenikky416@gmail.com>
2026-02-04 19:48:45 +0000

    update tests

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -6,11 +6,11 @@
 import RegisterPage from '../support/pages/RegisterPage';
 
 describe('Registration Page UI Tests', () => {
-  beforeEach(function() {
-     cy.visit('');
-     cy.fixture('registerData.json').as('registerData')
+  beforeEach(function () {
+    cy.visit('');
+    cy.fixture('registerData.json').as('registerData')
   });
-  
+
 
   it("Verify User can navigate to the Registration Page", () => {
     RegisterPage.clickRegisterButton();
@@ -19,41 +19,41 @@ describe('Registration Page UI Tests', () => {
 
   it("Verify Error message on blank Resgistration Submission", () => {
     RegisterPage.clickRegisterButton();
-    RegisterPage.clickRegbtn({delay:50});
+    RegisterPage.clickRegbtn({ delay: 50 });
     //assertion
     RegisterPage.verifyErrorMessage().invoke('text').then((text) => {
       expect(text.trim()).to.contain('All fields are required.');
     })
   });
 
-    it('Verify Registration Page Element Exist', () => {
-      RegisterPage.clickRegisterButton();
-      //RegisterPage.verifyElements().should('exist');
+  it('Verify Registration Page Element Exist', () => {
+    RegisterPage.clickRegisterButton();
+    //RegisterPage.verifyElements().should('exist');
   });
 
-  it('Verify Registration with valid details', function() {
+  it('Verify Registration with valid details', function () {
+    const uniqueEmail = `valid_user_${Date.now()}@example.com`;
     RegisterPage.clickRegisterButton();
-    RegisterPage.register(this.registerData.validUser.name, this.registerData.validUser.email, this.registerData.validUser.password, this.registerData.validUser.confirmPassword);
+    RegisterPage.register(this.registerData.validUser.name, uniqueEmail, this.registerData.validUser.password, this.registerData.validUser.confirmPassword);
     RegisterPage.pageElements.successMessage().should('be.visible').and('contain', 'Registered successfully');
-    
-  });  
-  it('Verify registration with mismatched passwords', function() {
+  });
+  it('Verify registration with mismatched passwords', function () {
     RegisterPage.clickRegisterButton();
     RegisterPage.register(this.registerData.unmatchedPassword.name, this.registerData.unmatchedPassword.email, this.registerData.unmatchedPassword.password, this.registerData.unmatchedPassword.confirmPassword);
     RegisterPage.verifyErrorMessage().should('contain', 'Passwords do not match');
   });
- it('Verify registration with Invalid Password Length', function() {
+  it('Verify registration with Invalid Password Length', function () {
     RegisterPage.clickRegisterButton();
     RegisterPage.register(this.registerData.invalidPasswordLength.name, this.registerData.invalidPasswordLength.email, this.registerData.invalidPasswordLength.password, this.registerData.invalidPasswordLength.confirmPassword);
     RegisterPage.verifyErrorMessage().should('contain', 'Password must be min 8 characters long and include uppercase, lowercase, number, and symbol');
   });
-  it('Verify registration with already registered email', function() {
+  it('Verify registration with already registered email', function () {
     RegisterPage.clickRegisterButton();
     RegisterPage.register(this.registerData.validUser.name, this.registerData.validUser.email, this.registerData.validUser.password, this.registerData.validUser.confirmPassword);
     RegisterPage.verifyErrorMessage().should('contain', 'User already exists');
   });
-  
 
-  
-  
+
+
+
 });
