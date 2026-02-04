d0d9141e3470997ff5e8105b199560fbfdde7e3e
AdenikeDavis <adenikky416@gmail.com>
2026-02-04 16:48:55 +0000

    update

--- cypress/support/pages/LoginPage.js
+++ cypress/support/pages/LoginPage.js
@@ -4,20 +4,20 @@
  */
 class LoginPage {
   // Selectors
-  elements= {
-   emailInput : () => cy.get('input[name="email"]'),
-    passwordInput : () => cy.get('input[name="password"]'),
-  loginButton : () => cy.contains('button', 'Login'),
-  errorMessage : () => cy.get('div.bg-red-50'),
-  successMessage : () => cy.get('div.bg-green-50'),
-  pageTitle : () => cy.get('.text-2xl'),
-  logoutBtn: ()=> cy.contains('button', 'Logout'),
-  createShift: () => cy.get('button[type="button"]').contains('Create Shift'),
-  shiftBackButton: () => cy.contains('button', 'Back to Shifts')
-  
-}
+  elements = {
+    emailInput: () => cy.get('input[name="email"]', { timeout: 10000 }),
+    passwordInput: () => cy.get('input[name="password"]'),
+    loginButton: () => cy.contains('button', 'Login'),
+    errorMessage: () => cy.get('div.bg-red-50'),
+    successMessage: () => cy.get('div.bg-green-50'),
+    pageTitle: () => cy.get('.text-2xl'),
+    logoutBtn: () => cy.contains('button', 'Logout'),
+    createShift: () => cy.get('button[type="button"]').contains('Create Shift'),
+    shiftBackButton: () => cy.contains('button', 'Back to Shifts')
 
-  
+  }
+
+
   verifyPageLoaded() {
     const el = this.elements;
     return (
@@ -39,39 +39,39 @@ class LoginPage {
     return this.elements.pageTitle();
   }
 
-  
+
   clickLoginButton() {
-    this.elements.loginButton().click({delay:50});
+    this.elements.loginButton().click({ delay: 50 });
   }
-  login(email, password){
+  login(email, password) {
     this.elements.emailInput().type(email);
     this.elements.passwordInput().type(password);
     this.clickLoginButton();
   }
 
-  
-clickLogoutButton() {
-  this.elements.logoutBtn().click();
-}
-getLogoutButton() {
-  return this.elements.logoutBtn();
-}
-getLoginButton() {
-  return this.elements.loginButton();
-}
-  
-  verifyErrorMessage() {
-   return this.elements.errorMessage();
+
+  clickLogoutButton() {
+    this.elements.logoutBtn().click();
+  }
+  getLogoutButton() {
+    return this.elements.logoutBtn();
+  }
+  getLoginButton() {
+    return this.elements.loginButton();
   }
 
- 
+  verifyErrorMessage() {
+    return this.elements.errorMessage();
+  }
+
+
   verifySuccessMessage() {
     return this.elements.successMessage();
   }
-  clickCreateShift(){
+  clickCreateShift() {
     this.elements.createShift().click();
   }
-  clickBackToShifts(){
+  clickBackToShifts() {
     this.elements.shiftBackButton().click();
   }
 
